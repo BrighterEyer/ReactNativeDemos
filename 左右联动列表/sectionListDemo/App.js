4d718a0f3f0ae6f80d7efa943b58bf0377f3d766
sibalou <lanlonggu@foxmail.com>
2019-09-13 11:03:58 +0800

    + SectionList左右联动列表

---
+++ 左右联动列表/sectionListDemo/App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+import ParcelPage from './src/ParcelPage';
+
+const App = () => {
+  return (
+    <ParcelPage/>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
