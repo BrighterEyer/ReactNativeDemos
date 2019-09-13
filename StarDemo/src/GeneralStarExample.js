4dc23a585750f411f5e0686768a5d7e941f50709
sibalou <lanlonggu@foxmail.com>
2019-09-13 08:43:16 +0800

    + 评价星号

---
+++ StarDemo/src/GeneralStarExample.js
@@ -0,0 +1,33 @@
+
+import React, {Fragment} from 'react';
+
+import StarRating from 'react-native-star-rating';
+
+class GeneralStarExample extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            starCount: 3.5
+        };
+    }
+
+    onStarRatingPress(rating) {
+        this.setState({
+            starCount: rating
+        });
+    }
+
+    render() {
+        return (
+            <StarRating
+                disabled={false}
+                maxStars={5}
+                rating={this.state.starCount}
+                selectedStar={(rating) => this.onStarRatingPress(rating)}
+            />
+        );
+    }
+}
+
+export default GeneralStarExample
