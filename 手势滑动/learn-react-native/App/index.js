76bf9829e4e603538dd177502c40b5e13a9ec4aa
sibalou <lanlonggu@foxmail.com>
2019-09-13 08:45:04 +0800

    + 手势滑动 横向列表

---
+++ 手势滑动/learn-react-native/App/index.js
@@ -0,0 +1,14 @@
+// import SimpleSwiper from './0_SimpleSwiper'
+// export default SimpleSwiper;
+
+
+// import AnimatedSwiper from './1_AnimatedSwiper'
+// export default AnimatedSwiper;
+
+
+// import GestureSwiper from './2_GestureSwiper'
+// export default GestureSwiper;
+
+
+import SmartSwiper from './3_SmartSwiper'
+export default SmartSwiper;
